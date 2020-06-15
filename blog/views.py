0a06e6266369b37db9140bf42a0189b0de6cd3fa
innertime <innertime@naver.com>
2020-06-16 00:16:12 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):                                             # Create your views here.
+    return render(request, 'blog/post_list.html', {})
