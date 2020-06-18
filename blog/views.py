a4558d09a729ef142bec6398050c2d914d545f8a
innertime <innertime@naver.com>
2020-06-18 16:27:26 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.utils import timezone                                   # timezone 모듈을 불러와야하니
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):                                             # Create your views here.
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')      # posts 퀴리셋의 이름
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
                                                                     # 매개변수 request(사용자가 요청하는 모든 것)
                                                                     # 'blog/post_list.html' 템플릿
                                                                     # {} - 이곳에 템플릿을 사용하기 위해 매개변수를 추가할 거에요.
                                                                     # (이 매개변수를'posts'라고 할거에요){'posts': posts}이렇게 작성할거에요.
-                                                                    #  :이전에 문자열이 와야하고, 작은 따옴표'' 
-
+                                                                    #  :이전에 문자열이 와야하고, 작은 따옴표''
